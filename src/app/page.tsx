cb3e35208700744f68dbeaaa8bc570c4eff7e46b
ヒロシ <japont110@gmail.com>
2025-04-24 13:40:44 +0900

    クリックイベント記述した

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
+"use client";
 import Image from "next/image";
 import Flexbox from "../components/Flexbox";
 import styles from "./page.module.css";
+import { useCallback } from "react";
 
 export default function Home() {
+  const foo = 1;
+
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  }, []);
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <a href="/about" onClick={handleClick}>
+          ボタン
+        </a>
         <Image
           className={styles.logo}
           src="/next.svg"
