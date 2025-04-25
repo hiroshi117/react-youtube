3612317ef01c5712afa3b829a8135d29b2b9e6bb
ヒロシ <japont110@gmail.com>
2025-04-25 10:04:08 +0900

    usestate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,23 +3,22 @@
 import Image from "next/image";
 import Flexbox from "../components/Flexbox";
 import styles from "./page.module.css";
-import { useCallback } from "react"; // React は型で使うので import は不要でもOK
 
 export default function Home() {
-  const foo = 1;
+  // const foo = 1;
 
-  const handleClick = useCallback((e: React.MouseEvent<HTMLAnchorElement>) => {
-    e.preventDefault();
-    console.log(e.currentTarget.href);
-    alert(foo);
-  }, []);
+  // const handleClick = useCallback((e: React.MouseEvent<HTMLAnchorElement>) => {
+  //   e.preventDefault();
+  //   console.log(e.currentTarget.href);
+  //   alert(foo);
+  // }, []);
 
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <a href="/about" onClick={handleClick}>
+        {/* <a href="/about" onClick={handleClick}>
           button
-        </a>
+        </a> */}
         <Image
           className={styles.logo}
           src="/next.svg"
--- src/components/Flexbox/index.tsx
+++ src/components/Flexbox/index.tsx
@@ -1,4 +1,5 @@
 import styles from "./Flexbox.module.css";
+import { useEffect } from "react"; // React は型で使うので import は不要でもOK
 
 const ITEMS = [
   { href: "https://x.com/", title: "X", description: "Xだよ" },
@@ -20,6 +21,16 @@ const ITEMS = [
 ];
 
 export default function Flexbox() {
+  useEffect(() => {
+    console.log("マウント");
+    document.body.style.backgroundColor = "lightblue";
+
+    return () => {
+      console.log("annマウント");
+      document.body.style.backgroundColor = "";
+    };
+  }, []);
+
   return (
     <div className={styles.flexbox}>
       {ITEMS.map((item) => {
