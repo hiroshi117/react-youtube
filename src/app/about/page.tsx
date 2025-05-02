3fcc5a08639498a0bbff72ca971b441f6d057319
ヒロシ <japont110@gmail.com>
2025-05-02 10:53:08 +0900

    useMemo

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,13 +6,14 @@ import { useInputArray } from "@/hooks/useInputArray";
 import { useBgLightBlue } from "@/hooks/useBgLightBlue";
 
 export default function AboutPage() {
-  const { count, isShow, handleClick, handleDisplay } = useCounter();
+  const { count, doubleCount, isShow, handleClick, handleDisplay } =
+    useCounter();
   const { text, array, handleChange, handleAdd } = useInputArray();
   useBgLightBlue();
 
   return (
     <div className={styles.page}>
-      {isShow ? <h1>{count}</h1> : null}
+      {isShow ? <h1>{doubleCount}</h1> : null}
       <button onClick={handleClick}>ボタン</button>
       <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
       <input type="text" value={text} onChange={handleChange} />
